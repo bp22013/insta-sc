4b449ae27ae0105071297f320fed7c28440b03ec
bp22013 <118529334+bp22013@users.noreply.github.com>
2024-03-03 18:37:17 +0900

    Delete userinfo directory

--- userinfo/models.py
+++
@@ -1,9 +0,0 @@
-# userinfo/models.py
-
-from django.db import models
-
-class InstagramUser(models.Model):
-    username = models.CharField(max_length=255)
-    full_name = models.CharField(max_length=255)
-    biography = models.TextField()
-    profile_pic_url = models.URLField()
