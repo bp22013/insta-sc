3dd2906898bb846df3d5a2a054f809cdb01fc33d
bp22013 <118529334+bp22013@users.noreply.github.com>
2024-03-03 20:36:06 +0900

    Update insta.py

--- insta.py
+++ insta.py
@@ -1,29 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
 from instaloader import Instaloader, Profile
 
 app = Flask(__name__)
 L = Instaloader()
 
-@app.route('/')
-def home():
-    return render_template('form.html')
-
-@app.route('/download_profile', methods=['POST'])
+@app.route('/download_profile', methods=['GET'])
 def download_profile():
-    id = request.form['id']
     try:
+        id = request.args.get('id')
         profile = Profile.from_username(L.context, id)
-        profile_info = {
-            'username': profile.username,
-            'full_name': profile.full_name,
-            'posts': profile.mediacount,
-            'followers': profile.followers,
-            'followees': profile.followees,
-            'profile_pic_url': profile.profile_pic_url
+        user_info = {
+            "username": profile.username,
+            "full_name": profile.full_name,
+            "posts": profile.mediacount,
+            "followers": profile.followers,
+            "followees": profile.followees
         }
-        return render_template('profile.html', profile=profile_info)
+        return jsonify(user_info)
     except Exception as e:
-        return str(e)
+        return jsonify({"error": str(e)}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
